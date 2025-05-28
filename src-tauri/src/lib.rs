920bb182347c361e8978392e0be49914774d828d
Chasen Liu <xsro@foxmail.com>
2025-05-28 16:06:04 +0800

    完成脚手架

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,11 @@
+use mas::version;
+use tauri::http::version;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let version = version();
+    format!("Hello, {}! You've been greeted from Rust! with {}", name,version)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
